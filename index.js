9fc29e31d75e4ab7a8409a8d74d180a81bd1e90d
GisellaAballay <gisellaaballaybonilla@gmail.com>
2024-09-22 19:05:34 +0200

    create handleUserObjetc in index.js

--- index.js
+++ index.js
@@ -3,13 +3,27 @@
 // 3º evaluar que accion quiere realizar el usuario (list, search/get, add, update, delete)
 // 4º devolver el output al cliente final
 
-import { getUsers, addUsers, updatedUser, deleteUser } from './models.js'; 
 import { createUserObject, createUpdateUserObject } from './utils/createObjectUser';
 
+import { getUsers, addUsers, updatedUser, deleteUser } from '.models.js'; 
+
+// 1º Recibir los argumentos pasados por la terminal
 
 const action = process.argv[2];  
 const args = process.argv.slice(3); 
 
+// 2º Funcion para manejar la creación o actualización de usuarios
+
+const handleUserObject = (action, args) => {
+  if (action === 'add') {
+    return createUserObject(args);  // Si es agregar usuario, se envían los argumentos
+  } else if (action === 'update') {
+    return createUpdateUserObject(args);  // Si es para actualizar
+  }
+  return null;
+};
+
+// 3º
 switch (action) {
   case 'createUser':
     createUserObject(args);
